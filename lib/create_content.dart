5e9b0a3f1bf387e3d571fb47837eda00e32ac06e
ziyou2023 <ziyouwu2023@gmail.com>
2023-05-19 13:23:23 +0800

    add images file and assets

--- lib/create_content.dart
+++ lib/create_content.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ziyou_math_app_flutter/mongodb.dart';
 import 'package:ziyou_math_app_flutter/ziyou_api.dart';
 
+
 TextEditingController titleController = TextEditingController();
 TextEditingController contentController = TextEditingController();
 
@@ -16,6 +17,9 @@ class CreatePage extends StatefulWidget {
 
 class _CreatePage extends State<CreatePage> {
 
+  int _numberOfQuestion = 0;
+  int _numberOfDifficulty = 2;
+
   Future<void> _btnSave(String title, String content, BuildContext context) async{
     var dataMap = <String, dynamic>{
       'title':title,
@@ -28,6 +32,110 @@ class _CreatePage extends State<CreatePage> {
 
   }
 
+  Widget buildNumberOfQuestions(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const SizedBox(width: 10),
+        Text(
+          "題數",
+          style: Theme.of(context).primaryTextTheme.bodyMedium,
+        ),
+        const SizedBox(width: 10),
+        IconButton(
+          icon: const Icon(Icons.remove),
+          onPressed: () {
+            if (_numberOfQuestion != 0) {
+              setState(() {_numberOfQuestion--;});
+            }
+          }
+        ),
+        const SizedBox(width: 10),
+        Text(
+          "$_numberOfQuestion",
+          style: Theme.of(context).primaryTextTheme.bodyMedium,
+        ),
+        const SizedBox(width: 10),
+        IconButton(
+          icon: const Icon(Icons.add),
+          onPressed: () {
+              setState(() {_numberOfQuestion++;});
+          }
+        ),
+        const SizedBox(width: 10),
+      ],
+    );
+  }
+
+  // Widget buildDifficulty(BuildContext context) {
+  //   return Row(
+  //     mainAxisAlignment: MainAxisAlignment.center,
+  //     children: <Widget>[
+  //       RadioListTile(
+  //         value: 1,
+  //         selected: true,
+  //         groupValue: _numberOfDifficulty,
+  //         controlAffinity: ListTileControlAffinity.leading,
+  //         activeColor: Theme.of(context).colorScheme.inversePrimary,
+  //         title: Text(
+  //           "簡單",
+  //           style: Theme.of(context).primaryTextTheme.bodyMedium,
+  //         ),
+  //         onChanged: (value) {
+  //           setState(() {_numberOfDifficulty = value!;});
+  //         }
+  //       ),
+  //       RadioListTile(
+  //           value: 2,
+  //           selected: true,
+  //           groupValue: _numberOfDifficulty,
+  //           controlAffinity: ListTileControlAffinity.leading,
+  //           activeColor: Theme.of(context).colorScheme.inversePrimary,
+  //           title: Text(
+  //             "中等",
+  //             style: Theme.of(context).primaryTextTheme.bodyMedium,
+  //           ),
+  //           onChanged: (value) {
+  //             setState(() {_numberOfDifficulty = value!;});
+  //           }
+  //       ),
+  //       RadioListTile(
+  //           value: 3,
+  //           selected: true,
+  //           groupValue: _numberOfDifficulty,
+  //           controlAffinity: ListTileControlAffinity.leading,
+  //           activeColor: Theme.of(context).colorScheme.inversePrimary,
+  //           title: Text(
+  //             "困難",
+  //             style: Theme.of(context).primaryTextTheme.bodyMedium,
+  //           ),
+  //           onChanged: (value) {
+  //             setState(() {_numberOfDifficulty = value!;});
+  //           }
+  //       )
+  //     ],
+  //   );
+  // }
+  //
+  // Widget buildBtnLastProblem(BuildContext context) {
+  //   return IconButton(onPressed: onPressed, icon: icon);
+  // }
+  //
+  // Widget buildBtnNextProblem(BuildContext context) {
+  //   return IconButton(
+  //     icon: const Icon(Icons.chevron_right_rounded),
+  //     onPressed: (){});
+  // }
+  //
+  // Widget buildImgProblem(BuildContext context) {
+  //   return Center(
+  //
+  //   );
+  // }
+
+  // Image.network(
+  // 'https://api.emath.math.ncu.edu.tw/problem/9ab8a119b144f0040bfc0490a5b7c30e')
+  //
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,26 +150,30 @@ class _CreatePage extends State<CreatePage> {
             children: <Widget>[
               Padding(
                 padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-                child: TextField(
-                  controller: titleController,
-                  autofocus: true,
-                  decoration: const InputDecoration(
-                    labelText: "Title",
-                    hintText: "The title is...",
-                  ),
+                child: Row(
+                  children: <Widget>[
+                    // todo:難易度->RadioListTile
+                    // buildDifficulty(context),
+                    const SizedBox(width: 20,),
+                    // todo: 題數 => finish
+                    buildNumberOfQuestions(context),
+                  ],
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.fromLTRB(20, 20, 20, 40),
-                child: TextField(
-                  controller: contentController,
-                  maxLines: 5,
-                  decoration: const InputDecoration(
-                    labelText: "Content",
-                    hintText: "Write something...",
-                  ),
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                child: Row(
+                  children: <Widget>[
+                    // todo: 前一題-> button
+                    // buildBtnLastProblem(context),
+                    // todo: 題目圖片
+                    // buildImgProlem(context),
+                    // todo: 後一題-> button
+                    // buildBtnNextProblem(context),
+                  ],
                 ),
               ),
+
               ElevatedButton(
                 style: Theme.of(context).elevatedButtonTheme.style,
                 child: const Text('Save'),
@@ -78,3 +190,26 @@ class _CreatePage extends State<CreatePage> {
     );
   }
 }
+
+// Padding(
+// padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+// child: TextField(
+// controller: titleController,
+// autofocus: true,
+// decoration: const InputDecoration(
+// labelText: "Title",
+// hintText: "The title is...",
+// ),
+// ),
+// ),
+// Padding(
+// padding: const EdgeInsets.fromLTRB(20, 20, 20, 40),
+// child: TextField(
+// controller: contentController,
+// maxLines: 5,
+// decoration: const InputDecoration(
+// labelText: "Content",
+// hintText: "Write something...",
+// ),
+// ),
+// ),
--- lib/ziyou_api.dart
+++ lib/ziyou_api.dart
@@ -4,7 +4,11 @@ import 'package:http/http.dart' as http;
 
 
 class ZiYouAPI {
+
   final String url = "https://api.emath.math.ncu.edu.tw/problem/serial/";
+  // ProblemID: 版本+年級+學期+章節+難易度
+  // todo: 後三碼的意義
+  final String problemId = "312043001";
   final Map<String, String> header = {
     "Authorization": "5UDGLP+gy9zaD2VwlLI7jjaxZ4+QIxSpOUXKgSD3KHR5wz4i7BktKFIlfAP2",
     "Accept": "application/json"
@@ -13,7 +17,7 @@ class ZiYouAPI {
   Future<void> getProblem() async {
     try {
       var response = await http.get(
-          Uri.parse("https://api.emath.math.ncu.edu.tw/problem/random/range/1/1/1/1/1"),
+          Uri.parse('https://api.emath.math.ncu.edu.tw/problem/serial/111011001/10'),
           headers: header
       );
       if (response.body.isNotEmpty) {
